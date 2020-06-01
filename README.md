22c13eaab38ad648d2b41936d5b353312ffac098
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-06-01 13:50:29 -0400

    init.py, update home

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # twitoff_Banks
 Twitoff project
+
+## Installation
+
+TODO: instruction for git clone
+
+## Setup
+
+TODO: instruction for virtual environment
+
+## Usage
+
+```sh
+#Mac
+FLASK_APP=hello.py flask run
+
+#Windows
+
+export FLASK_APP=hello.py # one-time thing, to set the env var flask run
+```
---
+++ flask_app/routes/home_routes.py
@@ -0,0 +1,15 @@
+# flask_app/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route('/')
+def hello_world():
+    print("You visited the homepage")
+    return 'Hello, World!'
+
+@home_routes.route('/about')
+def about():
+    print("You visited the About-Me page")
+    return 'About Me (TODO)'
