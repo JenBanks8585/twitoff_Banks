68d9ffede6e9a9105253f14744ee5e3ac4318989
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-06-04 15:50:07 -0400

    storing user infor in database

--- flask_app/models.py
+++ flask_app/models.py
@@ -13,10 +13,28 @@ class Book(db.Model):
     title = db.Column(db.String(128))
     author_id = db.Column(db.String(128))
 
+# for creating new tweets
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     twit = db.Column(db.String(128))
     twit_user = db.Column(db.String(128))
+
+# for fetching twitter users and tweets
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    screen_name = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    followers_count = db.Column(db.Integer)
+    #latest_tweet_id = db.Column(db.BigInteger)
+
+class TweetMsg(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey("user.id"))
+    full_text = db.Column(db.String(500))
+    embedding = db.Column(db.PickleType)
+
+    user = db.relationship("User", backref=db.backref("tweets", lazy=True))
 
 
 def parse_records(database_records):    
--- flask_app/routes/twitter_routes.py
+++ flask_app/routes/twitter_routes.py
@@ -1,25 +1,37 @@
 # flask_app/routes/twitter_routes.py
 
 from flask import Blueprint, jsonify, render_template , request, flash, redirect
-from flask_app.models import db, Book, parse_records
+from flask_app.models import db, User, TweetMsg, parse_records
+
+from web_app.services.twitter_service import api as twitter_api
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
 @twitter_routes.route("/users/<screen_name>/fetch")
 
 def fetch_user_data(screen_name):
-    print(screen_name)
+    print("FETCHING...",screen_name)
 
-    # TODO: fetch user info
+    # fetch user info
+    user= twitter_api.get_user(screen_name)
 
     # TODO: store user infor in database
+    db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
+    db_user.screen_name=twitter_user.screen_name
+    db_user.name=twitter_user.name
+    db_user.location=twitter_user.location
+    db_user.followers_count=twitter_user.followers_count
 
-    # TODO: fetch their tweets
+    db.session.add(db_user)
+    db.session.commit()
+
+    # fetch their tweets
+    statuses = twitter_api.user_timeline("elonmusk", count = 35)
 
     # TODO: fetch embeddings for each tweet
 
     # TODO: store tweets in database (w/embeddings)
 
 
-    return f"FETCHED {screen_name} OK"
+    return jsonify({"user": user._json, "num_tweets": len(statuses)})
 
