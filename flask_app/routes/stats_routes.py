9808dd2e2715c6e9d9caced922d9a209ba8eac78
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-06-07 11:34:17 -0400

    pretraining model, write and read pickle file

--- flask_app/routes/stats_routes.py
+++ flask_app/routes/stats_routes.py
@@ -1,16 +1,15 @@
 from flask import Blueprint, jsonify, request , render_template, flash, redirect
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
-from flask_app.models import db
+from flask_app.statsmodels import load_model
 
 stats_routes = Blueprint("stats_routes", __name__)
 
 
-@stats_routes.route('/stats/iris')
+@stats_routes.route("/stats/iris")
 def iris(): 
     X, y = load_iris(return_X_y=True)
-    clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial')
-    clf.fit(X, y)
+    clf = load_model()
     result = str(clf.predict(X[:2, :]))
     print("PREDICTION", result)
     return result
